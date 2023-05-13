c4b2bcdef9159691727c134a11d390f8e2f70d63
Even Tang <eventang2015@outlook.com>
2023-05-13 14:22:09 +0800

    add collection link

--- src/components/pages/featured-collections.js
+++ src/components/pages/featured-collections.js
@@ -1,4 +1,5 @@
 import { searchCollections } from "@/lib/api"
+import Link from 'next/link'
 
 export default async function FeaturedCollections() {
     const collectionsData = await searchCollections({ page_index: 1, page_size: 10, sort_by: 'desc', order_by: 'total_supply' })
@@ -11,20 +12,22 @@ export default async function FeaturedCollections() {
             </div>
             <ul role="list" className="divide-y divide-gray-100">
                 {collectionsData.items.map((item) => (
-                    <li key={item.key} className="flex justify-between gap-x-6 py-5">
-                        <div className="flex gap-x-4">
-                            <div className="min-w-0 flex-auto">
-                                <p className="text-sm font-semibold leading-6 text-gray-900">{item.name}</p>
-                                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{item.contract_address}</p>
+                    <Link as={`/collections/${item.contract_address}`} href="/collections/[address]">
+                        <li key={item.key} className="flex justify-between gap-x-6 py-5">
+                            <div className="flex gap-x-4">
+                                <div className="min-w-0 flex-auto">
+                                    <p className="text-sm font-semibold leading-6 text-gray-900">{item.name}</p>
+                                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{item.contract_address}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="hidden sm:flex sm:flex-col sm:items-end">
-                            <p className="text-sm leading-6 text-gray-900">{item.symbol}</p>
-                            <p className="mt-1 text-xs leading-5 text-gray-500">
-                                <time dateTime={item.created}>{item.created}</time>
-                            </p>
-                        </div>
-                    </li>
+                            <div className="hidden sm:flex sm:flex-col sm:items-end">
+                                <p className="text-sm leading-6 text-gray-900">{item.symbol}</p>
+                                <p className="mt-1 text-xs leading-5 text-gray-500">
+                                    <time dateTime={item.created}>{item.created}</time>
+                                </p>
+                            </div>
+                        </li>
+                    </Link>
                 ))}
             </ul>
         </div>
