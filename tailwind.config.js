c230a4554fea5cc92605da9deb3c6dc74cb93303
Shayne Brady <hello@shaynebrady.com>
2023-07-12 14:05:22 +1000

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,21 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    transparent: "transparent",
+    current: "currentColor",
     extend: {
+      colors: {
+        "text-error": "#FF4B5C",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      }
     },
     backgroundColor: {
       "dark-90": "#1C1C1C",
@@ -21,9 +28,10 @@ module.exports = {
     },
     colors: {
       "grey-80": "#A9A9A9",
+      "text-error": "#FF4B5C",
       white: "#FFFFFF",
       black: "#000000",
     },
   },
-  plugins: [],
+  plugins: [require("@headlessui/tailwindcss")],
 };
