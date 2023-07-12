a850acc65c5df261012b40b9e4befbf739d965f5
Shayne Brady <hello@shaynebrady.com>
2023-07-12 14:16:43 +1000

    cleanup collection and nft grids

--- src/components/collections-table.tsx
+++ src/components/collections-table.tsx
@@ -1,24 +1,15 @@
-import {
-  Card,
-  Table,
-  TableHead,
-  TableRow,
-  TableHeaderCell,
-  TableBody,
-  TableCell,
-  Title,
-  Text,
-} from "@tremor/react";
-import Link from "next/link";
+
 import { Collection } from "@/interfaces/ICollection";
 import { CollectionCard } from "./collection-card";
 
-export default function CollectionsTable({ items }: { items: Collection[] }) {
+export default function CollectionsTable({ items, cols = 5 }: { items: Collection[], cols?: number }) {
   return (
-    <div className="flex flex-wrap gap-5 mt-8">
-      {items.map((item: Collection, index: number) => (
+    <div className={`grid grid-cols-${cols} gap-5 mt-8`}>
+    {items.map((item, index: number) => (
+      <div className=" flex flex-wrap">
         <CollectionCard item={item} key={index} />
-      ))}
-    </div>
+      </div>
+    ))}
+  </div>
   );
 }
