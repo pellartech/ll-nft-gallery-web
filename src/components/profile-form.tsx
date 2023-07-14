a81f14c786356d1afc93b201decfd7873d3bd504
Shayne Brady <hello@shaynebrady.com>
2023-07-14 17:15:11 +1000

    use profile avatar

--- src/components/profile-form.tsx
+++ src/components/profile-form.tsx
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from 'yup'
-import { updateProfile } from '@/lib/api'
+import { updateProfile, updateProfileAvatar } from '@/lib/api'
 import Input from './input'
 import { useRouter } from 'next/navigation'
 import Button from './button'
 import FileInput from './file-input'
+import { getImage } from '@/utils/utils'
+import { IUser } from '@/interfaces/IUser'
 
-type FormData = {
-    name?: string,
-    bio?: string,
-    wallet_address?: string,
-    twitter?: string,
-    instagram?: string,
-    discord?: string
-}
-
-export default function ProfileForm({ user }: { user?: FormData }) {
+export default function ProfileForm({ user }: { user?: IUser }) {
+    const [loading, setLoading] = useState(false)
+    const [avatar, setAvatar] = useState<File | null>()
     const router = useRouter()
+
+    const originalAvatar = useMemo(() => {
+        console.log('user: ', user)
+        const url =  getImage(user?.avatar?.original) || '/images/profile_thumb.png'
+        console.log('url: ', url)
+        return url
+    }, [user])
 
     const schema = Yup.object().shape({
         name: Yup.string().required('Name is required')
@@ -28,29 +30,36 @@ export default function ProfileForm({ user }: { user?: FormData }) {
         resolver: yupResolver(schema)
     })
 
-    const onSubmit = async (data: FormData) => {
+    const onSubmit = async (data: IUser) => {
+        setLoading(true)
         const token = window.localStorage.getItem("lightlink-web-token")
         const result = await updateProfile(token, data.name, data.bio, data.twitter, data.instagram, data.discord)
         if (result) {
             router.push(`/profile/${user?.wallet_address}`)
+        } else {
+            setLoading(false)
         }
     }
+
+    const handleAvatarUpload = async (file: File) => {
+        const token = window.localStorage.getItem("lightlink-web-token")
+        await updateProfileAvatar(token, file)
+    }
+
+    useEffect(() => {
+        if (avatar) {
+            handleAvatarUpload(avatar)
+        }
+    }, [avatar])
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="space-y-12">
                 <div>
-                    <h2 className="text-base font-semibold leading-7 text-white">Your Details</h2>
+                    <FileInput imageClass="aspect-w-15 aspect-h-4 w-full rounded-xl" label="Image" accept="image/png, image/jpeg, image/jpg"  defaultImage="/images/profile_banner.png" onFileChange={(img) => { console.log(img) }} />
+                    <FileInput imageClass="h-[144px] w-[144px] rounded-full" label="Avatar" accept="image/png, image/jpeg, image/jpg"  defaultImage={originalAvatar} onFileChange={(img) => setAvatar(img)} />
 
-                    <div className="relative w-full" style={{ paddingBottom: "26.27%", marginBottom: '2rem' }}>
-                        <div className="absolute top-0 left-0 w-full h-full bg-blue-500">
-                            <FileInput label="Image" accept="image/png, image/jpeg, image/jpg" defaultImage="/images/profile_banner.png" onFileChange={(img) => { console.log(img) }} />
-                        </div>
-                    </div>
-
-                    <div className="h-[144px] w-[144px]">
-                        <FileInput label="Avatar" accept="image/png, image/jpeg, image/jpg" defaultImage="/images/profile_banner.png" onFileChange={(img) => { console.log(img) }} />
-                    </div>
+                    <h2 className="text-base font-semibold leading-7 text-white mt-4">Your Details</h2>
 
                     <Input type="text" label="Username" name="name" placeholder="Enter username" defaultValue={user?.name} register={register} error={errors.name?.message?.toString()} />
                     <Input type="textarea" label="Bio (optional)" placeholder="Add a few words about yourself" name="bio" defaultValue={user?.bio} register={register} error={errors.bio?.message?.toString()} />
@@ -65,7 +74,7 @@ export default function ProfileForm({ user }: { user?: FormData }) {
             </div>
 
             <div className="mt-6 flex items-center justify-end gap-x-6">
-                <Button type="submit">Save Edits</Button>
+                <Button loading={loading} type="submit">Save Edits</Button>
             </div>
         </form>
     )
--- src/modules/Profile/index.tsx
+++ src/modules/Profile/index.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { PageHeader, CollectionGrid, NftGrid } from "@/modules"
 import ExpandableText from "@/components/expandable-text"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { useAccount } from "wagmi"
 import Link from "next/link"
-import { formatAccountDisplay } from "@/utils/utils"
+import { formatAccountDisplay, getImage } from "@/utils/utils"
 import useSWR from "swr"
 import fetcher from "@/lib/fetcher"
 
@@ -17,28 +17,32 @@ const Profile = ({ address }: ProfileProps) => {
     const { address: connectedAddress } = useAccount();
     const fetchURL = `${process.env.NEXT_PUBLIC_API_URL_ROOT}/api/v1/users/${address}`
     const { data } = useSWR(fetchURL, fetcher)
+
+    const thumbImageUrl = useMemo(() => {
+        if (data?.user?.avatar?.original) {
+            return getImage(data?.user?.avatar?.original)
+        }
+    }, [data])
+
     return (
         <>
-            <PageHeader />
+            <PageHeader thumbUrl={thumbImageUrl} />
 
-            <div className="text-4xl text-white font-semibold mb-8 mt-80 flex justify-start gap-2 items-center">
+            <div className="text-4xl text-white font-semibold mb-4 mt-80 flex justify-start gap-2 items-center">
                 {data?.user?.name || formatAccountDisplay(address)}
 
                 {connectedAddress?.toLocaleLowerCase() === address.toLowerCase() &&
-                    <Link
-                        href="/profile/edit"
-                    >
+                    <Link href="/profile/edit">
                         <img className=" h-7" src="/images/icons/edit.svg" alt="" />
                     </Link>
-
                 }
             </div>
 
-            <div className="w-6/12 mb-8">
+            <div className="w-6/12 mb-4">
                 <ExpandableText text={data?.user?.bio || 'Bio has not been set'} limit={200} />
             </div>
 
-            <button type="button" className="flex gap-1 align-middle justify-center mb-8">
+            <button type="button" className="flex gap-1 align-middle justify-center mb-12">
                 <img className="m-auto" src="/images/icons/wallet.svg" alt="" />
                 <div className="m-auto text-white text-sm">{formatAccountDisplay(address)}</div>
             </button>
